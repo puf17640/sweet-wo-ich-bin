72e3007c54d54b6abcb3584723d76f6af64bf2d9
Julian Pufler <puf17640@spengergasse.at>
2021-04-13 14:20:06 +0200

    added fonts using fontsource and refactored a bit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "@fontsource/alatsi";
+import "@fontsource/alegreya-sans-sc";
+import "@fontsource/be-vietnam/800.css"
 
 export default function App({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,111 +1,117 @@
 import {
   Stack,
   Box,
-  Image,
   Heading,
   Button,
   Link,
   Text,
   Divider,
 } from "@chakra-ui/react";
+import Head from "next/head"
 import data from "../data";
-
-const MainButton = ({ title, url }) => {
-  return (
-    <Link
-      _hover={{ textDecoration: "none" }}
-      href={url}
-      title={title}
-      fontSize={"lg"}
-    >
-      <Button
-        colorScheme={"gray"}
-        w={"full"}
-        textTransform={"uppercase"}
-        letterSpacing={2}
-        fontFamily={"Be Vietnam, sans-serif"}
-        _hover={{
-          bgColor: "gray.300",
-        }}
-      >
-        {title}
-      </Button>
-    </Link>
-  );
-};
 
 export default function Home() {
   return (
-    <Box bg={"black"}>
-      <Stack
-        alignItems={"center"}
-        direction={"column"}
-        minH={"100vh"}
-        w={"full"}
-      >
+    <>
+      <Head>
+        <title>SWEET</title>
+      </Head>
+      <Box bg={"black"}>
         <Stack
-          flexGrow={1}
-          direction={{ base: "column", lg: "row" }}
-          justifyContent={"space-evenly"}
           alignItems={"center"}
+          direction={"column"}
+          minH={"100vh"}
           w={"full"}
-          py={4}
-          px={{ base: 4, md: 32 }}
-          spacing={{ base: 2, lg: 0 }}
         >
-          <Box
-            align={"center"}
-            maxW={"sm"}
-            w={"full"}
-            minH={{ base: "200px", md: "300px", lg: "400px" }}
-            maxH={{ base: "300px", md: "400px", lg: "500px" }}
+          <Stack
             flexGrow={1}
-            bg={"url(/static/profile.png)"}
-            backgroundPosition={"center"}
-            backgroundSize={"cover"}
-            rounded={"lg"}
-          ></Box>
-          <Box minW={{ lg: "300" }}>
-            <Heading
-              fontSize={"8xl"}
-              color={"whiteAlpha.900"}
+            direction={{ base: "column", lg: "row" }}
+            justifyContent={"space-evenly"}
+            alignItems={"center"}
+            w={"full"}
+            py={4}
+            px={{ base: 4, md: 32 }}
+            spacing={{ base: 2, lg: 0 }}
+          >
+            <Box
               align={"center"}
-              fontFamily={"Alatsi, sans-serif"}
-              letterSpacing={2}
-            >
-              SWEET
-            </Heading>
-            <Heading
-              fontSize={"3xl"}
-              color={"whiteAlpha.700"}
-              align={"center"}
-              fontFamily={"Alegreya Sans SC, sans-serif"}
-              letterSpacing={1}
-              mb={4}
-            >
-              Vienna
-            </Heading>
-            <Divider />
-            <Stack mt={6} spacing={2}>
-              {data.map((entry) => (
-                <MainButton key={entry.title} {...entry} />
-              ))}
-            </Stack>
-          </Box>
+              maxW={"sm"}
+              w={"full"}
+              minH={{ base: "200px", md: "300px", lg: "400px" }}
+              maxH={{ base: "300px", md: "400px", lg: "500px" }}
+              flexGrow={1}
+              bg={"url(/static/profile.png)"}
+              backgroundPosition={"center"}
+              backgroundSize={"cover"}
+              rounded={"lg"}
+            ></Box>
+            <Box minW={{ lg: "300" }}>
+              <Heading
+                as={Stack}
+                direction={"row"}
+                spacing={0}
+                alignItems={"center"}
+                justifyContent={"center"}
+                fontSize={"7xl"}
+                color={"whiteAlpha.900"}
+                align={"center"}
+                fontFamily={"Alatsi, sans-serif"}
+                letterSpacing={2}
+              >
+                <Box as={"span"} fontSize={"150%"}>$</Box>
+                <span>WEET</span>
+              </Heading>
+              <Heading
+                fontSize={"3xl"}
+                color={"whiteAlpha.700"}
+                align={"center"}
+                fontFamily={"Alegreya Sans SC, sans-serif"}
+                letterSpacing={1}
+                mb={4}
+              >
+                Vienna
+              </Heading>
+              <Divider />
+              <Stack mt={6} spacing={2}>
+                {data.map(({ title, url }) => (
+                  <Link
+                    _hover={{ textDecoration: "none" }}
+                    href={url}
+                    title={title}
+                    fontSize={"lg"}
+                    key={title}
+                  >
+                    <Button
+                      colorScheme={"gray"}
+                      w={"full"}
+                      textTransform={"uppercase"}
+                      letterSpacing={2}
+                      fontFamily={"Be Vietnam, sans-serif"}
+                      _hover={{
+                        bgColor: "gray.300",
+                      }}
+                    >
+                      {title}
+                    </Button>
+                  </Link>
+                ))}
+              </Stack>
+            </Box>
+          </Stack>
         </Stack>
-      </Stack>
-      <Text
-        align={"center"}
-        color={"whiteAlpha.500"}
-        fontWeight={"bold"}
-        p={2}
-        fontSize={"md"}
-      >
-        Website by{" "}
-        <Link target={"_blank"} href={"https://pufler.dev"} textDecoration={"underline"}>
-          Julian Pufler
-        </Link>
-      </Text>
-    </Box>
+        <Text
+          align={"center"}
+          color={"whiteAlpha.500"}
+          fontWeight={"bold"}
+          p={2}
+          fontSize={{ base: "sm", md: "md" }}
+        >
+          Website by{" "}
+          <Link target={"_blank"} href={"https://pufler.dev"} textDecoration={"underline"}>
+            Julian Pufler
+          </Link>
+        </Text>
+      </Box>
+    </>
   );
 }
